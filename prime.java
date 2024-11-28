61954eae8f24a42aa41313c4b67cd0d5d7787b77
Rahul Kongara <rahul.kongara13@gmail.com>
2024-11-28 10:45:25 +0530

    Commit-4: Changed number from 7 to 15

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class Prime {
     public static void main(String[] args) {
-        int number = 7; // Example number
+        int number = 15; // Example number
         boolean isPrime = true;
 
         for (int i = 2; i <= number / 2; ++i) {
