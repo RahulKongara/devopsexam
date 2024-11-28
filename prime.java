f7dd84040bc8c16464837f1846a6c5b1a70b2dd8
Rahul Kongara <rahul.kongara13@gmail.com>
2024-11-28 10:37:00 +0530

    Changed number from 29 to 25

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class Prime {
     public static void main(String[] args) {
-        int number = 29; // Example number
+        int number = 25; // Example number
         boolean isPrime = true;
 
         for (int i = 2; i <= number / 2; ++i) {
